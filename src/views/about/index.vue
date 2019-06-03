3b11204bfe3a612ede9cda9aa9ca8819ff690e0a
Tuffy <eliseguilian@gmail.com>
2019-06-03 15:23:34 +0800

    update about

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -6,13 +6,12 @@
             <img src="../../assets/img/user.jpg" alt="">
         </div>
         <div class="box-detail">
-            <h3>Tuffy</h3>
-            <p>ShenZhen.China</p>
-            <p>+86 18807800463</p>
-            <p>361329912@qq.com</p>
-            <p>Front End Engineer</p>
+            <h3>Tuffy - 阿胐</h3>
+            <p>Front End Engineer  / ShenZhen.China</p>
+            <p>361329912@qq.com  / +8618807800463</p>
+            <p>霍山有兽焉，装如狸，而白尾有鬣，</p>
+            <p>名曰胐胐，养之可以已忧。</p>
         </div>
-
     </div>
 </div>
 </template>
